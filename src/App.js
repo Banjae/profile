9743e635414c367f06b8185ed618e004f7d55c25
devking <brand0610@naver.com>
2023-01-26 13:15:52 +0900

    Router 기본구조 구성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { Route } from "react-router-dom";
+// common
+import Footer from "./components/common/Footer";
+import Header from "./components/common/Header";
+// main
+import Visual from "./components/main/Visual";
+import Content from "./components/main/Content";
+// sub
+import Department from "./components/sub/Department";
+import Community from "./components/sub/Community";
+import Join from "./components/sub/Join";
+import Location from "./components/sub/Location";
+import Youtube from "./components/sub/Youtube";
+import Gallery from "./components/sub/Gallery";
+
 function App() {
   return (
     <>
-      Hello
+      <Header />
+      <Route exact path="/">
+        <Visual />
+        <Content />
+      </Route>
+      {/* <Route path="/department">
+        <Department />
+      </Route> */}
+      <Route path="/department" component={Department} />
+      <Route path="/community" component={Community} />
+      <Route path="/join" component={Join} />
+      <Route path="/location" component={Location} />
+      <Route path="/youtube" component={Youtube} />
+      <Route path="/gallery" component={Gallery} />
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // App 컴포넌트를 BrowserRouter로 감싸서
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
