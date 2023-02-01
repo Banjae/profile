3f203f5baf79e9108e38ae0e29d1d13a2a516155
devking <brand0610@naver.com>
2023-02-01 10:19:31 +0900

    useEffect 및 useRef 활용

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 const Layout = (props) => {
+  const frame = useRef();
+
+  useEffect(() => {
+    frame.current.classList.remove("on");
+    frame.current.classList.add("on");
+    return () => {};
+  }, []);
+
   return (
-    <section className={`content ${props.title}`}>
+    <section className={`content ${props.title}`} ref={frame}>
       <figure></figure>
       <div className="inner">
         <h1>{props.title}</h1>
--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "../common/Layout";
 
 const Community = () => {
+
+  useEffect(() => {
+
+  }, [])
+
   return <Layout title={"Community"}>Community</Layout>;
 };
 
