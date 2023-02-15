0c630c0f368380351cf14ab6570581c65a4af2d9
devking <brand0610@naver.com>
2023-02-15 10:57:20 +0900

    nvio chart

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../common/Layout";
 import CommunityCard from "./CommunityCard";
+import axios from "axios";
+
+// XSS 공격 방어 라이브러리
+// https://www.npmjs.com/package/dompurify
+// npm i dompurify
+import DOMPurify from "dompurify";
+
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 // 01. useform import
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { ResponsivePie } from "@nivo/pie";
 
 // 02. form 요소 항목별 에러 체크 정의
 const schema = yup.object({
@@ -22,6 +32,8 @@ const Community = () => {
     register,
     handleSubmit,
     reset,
+    setValue,
+    trigger,
     formState: { errors },
   } = useForm({
     // yup과 연결
@@ -43,6 +55,7 @@ const Community = () => {
   const [allowed, setAllowed] = useState(true);
 
   const createPost = (data) => {
+    console.log(data);
     setPosts([...posts, data]);
     // 입력 저장후 초기화
     reset();
@@ -135,7 +148,6 @@ const Community = () => {
     e.preventDefault();
     if (e.target.files) {
       const uploadFile = e.target.files[0];
-      console.log(uploadFile);
 
       const reader = new FileReader();
       reader.readAsDataURL(uploadFile);
@@ -146,8 +158,278 @@ const Community = () => {
     }
   };
 
+  const quillRef = useRef(null);
+  const imageHandler = () => {
+    console.log("에디터에서 이미지 버튼을 클릭하면 이 핸들러가 작동");
+    // 1. 이미지를 저장할 input type="file" 을 생성한다.
+    const input = document.createElement("input");
+    // 2. 속성을 셋팅한다.
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    // 에디터 이미지버튼을 클릭하면 input 이 클릭되도록 한다.
+    input.click();
+    // input 이 클릭되면 선택창이 나온다.
+    // input 에 변화가 되면 이미지를 선택한 것이다.
+    input.addEventListener("change", async () => {
+      console.log("이미지 선택");
+      const file = input.files[0];
+      // 이미지를 변형한다.
+      const formData = new FormData();
+      // formData 의 형식은 키 - 벨류 구조
+      formData.append("img", file);
+      // formData 를 백엔드로 전송하고 주소를 받아온다.
+      // try {
+      //   const result = await axios.post(
+      //     "http://localhost:3000/images",
+      //     formData
+      //   );
+      //   console.log("성공시 , 백엔드가 보내주는 데이터", result.data.url);
+      //   // 명세서 확인 필요(url: http://127.0.123.124/image/a.gif)
+      //   const imgURL = result.data.url;
+      //   // ReactQuill Editer 에 html 수정해서 삽입
+      //   // userRef 를 이용해서 해당 요소를 선택한다.
+      //   const editer = quillRef.current.getEditor();
+      //   // 현재 에디터 커서 위치값을 파악
+      //   const range = editer.getSelection();
+      //   // 가져온 이미지를 삽입해서 출력한다.
+      //   editer.insertEmbed(range.index, "image", imgURL);
+      // } catch (error) {
+      //   console.log("이미지 전송 실패");
+      // }
+    });
+  };
+
+  // useMemo를 사용하지 않고 handler를 등록할 경우 타이핑 할때마다 focus가 벗어남
+  const modules = React.useMemo(
+    () => ({
+      toolbar: {
+        // container에 등록되는 순서대로 tool 배치
+        container: [
+          [{ font: [] }], // font 설정
+          [{ header: [1, 2, 3, 4, 5, 6, false] }], // header 설정
+          [
+            "bold",
+            "italic",
+            "underline",
+            "strike",
+            "blockquote",
+            "code-block",
+            "formula",
+          ], // 굵기, 기울기, 밑줄 등 부가 tool 설정
+          [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" },
+          ], // 리스트, 인덴트 설정
+          ["link", "image", "video"], // 링크, 이미지, 비디오 업로드 설정
+          [{ align: [] }, { color: [] }, { background: [] }], // 정렬, 글씨 색깔, 글씨 배경색 설정
+          ["clean"], // toolbar 설정 초기화 설정
+        ],
+
+        // custom 핸들러 설정
+        handlers: {
+          image: imageHandler, // 이미지 tool 사용에 대한 핸들러 설정
+        },
+      },
+    }),
+    []
+  );
+
+  // toolbar에 사용되는 tool format
+  const formats = [
+    "font",
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "code-block",
+    "formula",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+    "align",
+    "color",
+    "background",
+  ];
+
+  const handleChange = (value) => {
+    setTag(value);
+    // console.log(value);
+    // register 로 등록하지 않고, 강제로 넣어주는 기능
+    // 주의사항으로 "<p><br></p>" 이 내용 없는 것.
+    setValue("contents", value === "<p><br></p>" ? "" : value);
+    trigger("contents");
+  };
+
+  const [tag, setTag] = useState("<p><b>안녕</b></p>");
+
+  // 차트 데이터
+  const chartData = [
+    {
+      id: "lisp",
+      label: "lisp",
+      value: 511,
+      color: "hsl(174, 70%, 50%)",
+    },
+    {
+      id: "erlang",
+      label: "erlang",
+      value: 272,
+      color: "hsl(357, 70%, 50%)",
+    },
+    {
+      id: "c",
+      label: "c",
+      value: 160,
+      color: "hsl(154, 70%, 50%)",
+    },
+    {
+      id: "ruby",
+      label: "ruby",
+      value: 79,
+      color: "hsl(230, 70%, 50%)",
+    },
+    {
+      id: "javascript",
+      label: "javascript",
+      value: 443,
+      color: "hsl(45, 70%, 50%)",
+    },
+  ];
+
   return (
     <Layout title={"Community"}>
+      {/* 차트 출력 */}
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsivePie
+          data={chartData}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 0.2]],
+          }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{
+            from: "color",
+            modifiers: [["darker", 2]],
+          }}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              size: 4,
+              padding: 1,
+              stagger: true,
+            },
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          fill={[
+            {
+              match: {
+                id: "ruby",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "c",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "go",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "python",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "scala",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "lisp",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "elixir",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "javascript",
+              },
+              id: "lines",
+            },
+          ]}
+          legends={[
+            {
+              anchor: "bottom",
+              direction: "row",
+              justify: false,
+              translateX: 0,
+              translateY: 56,
+              itemsSpacing: 0,
+              itemWidth: 100,
+              itemHeight: 18,
+              itemTextColor: "#999",
+              itemDirection: "left-to-right",
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+        ;
+      </div>
+      {/* HTML 태그 출력 */}
+      <div
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(String(tag)) }}
+      />
       {/* 입력폼 */}
       <div className="inputBox">
         <form onSubmit={handleSubmit(createPost)}>
@@ -177,6 +459,14 @@ const Community = () => {
               accept="image/*"
               onInput={onChangeImg}
               ref={imgRef}
+            />
+          </div>
+
+          <div>
+            <ReactQuill
+              modules={modules}
+              formats={formats}
+              onChange={handleChange}
             />
           </div>
 
